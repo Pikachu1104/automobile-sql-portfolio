d8096db310d95e80a50d7b985e95b99c5f26bea2
Pikachu1104 <87816323+Pikachu1104@users.noreply.github.com>
2026-01-13 13:21:46 +0530

    Add models_master table to schema.sql

--- 01_schema/schema.sql
+++ 01_schema/schema.sql
@@ -34,4 +34,20 @@ CREATE TABLE vehicles (
     is_active BOOLEAN
 );
 
+--- Model Master Table ---
+CREATE TABLE models_master (
+    model_id INT PRIMARY KEY,
+    vehicle_model VARCHAR(50) NOT NULL,
+    vehicle_brand VARCHAR(50) NOT NULL,
+    grade VARCHAR(50) NOT NULL,
+    suffix VARCHAR(50) NOT NULL,
+    fuel_type VARCHAR(20) NOT NULL,
+    transmission VARCHAR(20) NOT NULL,
+    interior_color VARCHAR(50) NOT NULL,
+    exterior_color VARCHAR(50) NOT NULL,
+    base_price DECIMAL(12,2) NOT NULL,
+    launch_date DATE NOT NULL,
+    is_active BOOLEAN
+);
+
 
