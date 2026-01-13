340c5cf7b900a41aa665196fb670e8f16a48d10d
Pikachu1104 <87816323+Pikachu1104@users.noreply.github.com>
2026-01-13 13:01:56 +0530

    Update schema.sql

--- 01_schema/schema.sql
+++ 01_schema/schema.sql
@@ -1,1 +1,15 @@
--- Schema will be added here
+--- Customer Table ---
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_type VARCHAR(50),
+    customer_name VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    address VARCHAR(300) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    pincode VARCHAR(10) NOT NULL,
+    created_date DATE NOT NULL,
+    updated_date DATE NOT NULL,
+    is_active BOOLEAN
+);
+
